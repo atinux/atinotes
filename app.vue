dc9a7387d6a693defc51e7effe61efb96b8f05d9
Sébastien Chopin <seb@nuxtjs.com>
2023-02-09 10:09:03 +0100

    chore: css improvements on mobile

--- app.vue
+++ app.vue
@@ -10,6 +10,10 @@ body {
   margin: 0;
   background-color: #eee;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  padding-top: 20px;
+}
+@media (min-width: 600px) {
+  body {
+    padding-top: 20px;
+  }
 }
 </style>
