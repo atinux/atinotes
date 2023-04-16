f44475475bd489aefdc25155be30c4dbfd191eeb
Sébastien Chopin <seb@nuxt.com>
2023-04-16 13:47:13 +0200

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // Enable content to leverages <ContentRendererMarkdown /> component
-  modules: ['@nuxt/content', 'nuxt-og-image', '@nuxt/devtools'],
+  modules: ['@nuxt/content'],
+  devtools: {
+    enabled: true
+  },
   // Define storage used in API routes with useStorage()
   nitro: {
     storage: {
@@ -28,7 +31,7 @@ export default defineNuxtConfig({
       'postcss-nested': {}
     }
   },
-  ogImage: {
-    url: 'https://atinotes.nuxt.space'
-  }
+  // ogImage: {
+  //   url: 'https://atinotes.nuxt.space'
+  // }
 })
