16db2e9185f65721ae0fa1895ee0eeb8f15cee46
Sébastien Chopin <seb@nuxt.com>
2023-05-15 11:00:49 +0200

    Update stats.ts

--- server/api/stats.ts
+++ server/api/stats.ts
@@ -1,3 +1,5 @@
 export default defineEventHandler(async (event) => {
+  const keys = await useStorage('notes').getKeys() 
+  const settings = await useStorage('notes').getItem('settings')
   return await useStorage('notes').getItem('index')
 })
