012c1885f4d7d971ca7cfbdbe4f1b94eba6f75b3
Sébastien Chopin <seb@nuxt.com>
2023-10-25 13:16:53 +0200

    chore: update deps

--- components/OgImage/OgImagePage.vue
+++ components/OgImage/OgImagePage.vue
@@ -16,7 +16,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col justify-between items-start bg-white p-20 pt-24 pb-16 bg-gradient-to-br from-gray-50 to-gray-200">
+  <div class="og-image w-full h-full flex flex-col justify-between items-start bg-white p-20 pt-24 pb-16">
     <div>
       <h1 class="text-8xl mb-4 text-black flex items-center">
         {{ title }}
@@ -28,3 +28,9 @@ defineProps({
     <div class="font-bold text-4xl flex flex-row">atinotes<span class="text-gray-600">.pages.dev</span></div>
   </div>
 </template>
+
+<style scoped>
+.og-image {
+  background-image: linear-gradient(135deg, #fff 0%, #eee 100%);
+}
+</style>
